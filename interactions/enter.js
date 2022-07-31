e1c8b441c31a4b193a28ac5386497d6ef08b845b
bo <bobot.main@gmail.com>
2022-07-31 13:26:18 +0200

    entries.js done

---
+++ interactions/enter.js
@@ -0,0 +1,71 @@
+const fs = require("fs");
+const settings = require("../models/configurations.js");
+
+module.exports = {
+  name: "enter",
+  async interact(client, interaction) {
+    try {
+      await interaction.deferReply({ ephemeral: true });
+      const giveawaysConfigsDir = fs.readdirSync("./giveaways/giveawayConfigs");
+      const giveawaysEntriesDir = fs.readdirSync("./giveaways/giveawayEntries");
+      const giveawayConfigsFile = giveawaysConfigsDir.find((file) => file.includes(interaction.guildId) && file.includes(interaction.channelId) && file.includes(interaction.message.id));
+      const giveawayEntriesFile = giveawaysEntriesDir.find((file) => file.includes(interaction.guildId) && file.includes(interaction.channelId) && file.includes(interaction.message.id));
+      if (!giveawayEntriesFile || !giveawayConfigsFile) return interaction.editReply("invalid/unknown giveaway");
+      const giveawayConfig = fs.readFileSync(`./giveaways/giveawayConfigs/${giveawayConfigsFile}`, { encoding: 'utf8', flag: 'r' });
+      const giveawayEntries = fs.readFileSync(`./giveaways/giveawayEntries/${giveawayEntriesFile}`, { encoding: 'utf8', flag: 'r' });
+      let entries = giveawayEntries.split("\n");
+      if (entries.includes(interaction.member.id)) return interaction.editReply("You have already entered this giveaway.");
+      const configs = giveawayConfig.split("\n");
+      const roleConfigs = [configs[4], configs[5]];
+      const roles = interaction.member.roles.cache;
+      if (roleConfigs[0] !== "NA" && !roles.has(roleConfigs[0])) return interaction.editReply(`You do not have the required role to enter this giveaway - <@&${roleConfigs[0]}>.`);
+      if (roleConfigs[1] !== "NA" && roles.has(roleConfigs[1])) return interaction.editReply(`You are blacklisted from entering the giveaway since you have the blacklisted role - <@&${roleConfigs[1]}>.`);
+      let userEntries = 1;
+      const server_data = await settings.findOne({
+        server_id: interaction.guildId,
+      });
+      let roleApplicable = ``;
+      const roleEntries = server_data.roles;
+      roleEntries.forEach((roleArray) => {
+        const role = roleArray[0];
+        const entry = roleArray[1];
+        if (roles.has(role)) {
+          userEntries += entry;
+          roleApplicable += `<@&${role}> = ${entry} entries\n`;
+        };
+      });
+      for (i = 1; i <= userEntries; i++) {
+        entries.push(interaction.member.id);
+      };
+      let entriesString = entries.join("\n");
+      fs.writeFileSync(`./giveaways/giveawayEntries/${giveawayEntriesFile}`, entriesString);
+      let replyContent;
+      if (roleApplicable) {
+        replyContent = `You have successfully entered this giveaway!\nYou have a total of ${userEntries} entry/entries:\nCommon Entry = 1\n${roleApplicable}\nGoodluck! :slight_smile:`;
+      } else {
+        replyContent = `You have successfully entered this giveaway!\nYou have a total of ${userEntries} entry/entries:\nCommon Entry = 1\n\nGoodluck! :slight_smile:`;
+      };
+      return interaction.editReply({
+        content: replyContent,
+      });
+    } catch (e) {
+      console.log(e);
+      if (interaction.deferred || interaction.replied) {
+        await interaction.followUp({
+          content: "I am facing some issues , the dev has been informed . Please try again in some hours.",
+          embeds: [],
+          components: [],
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: "I am facing some issues , the dev has been informed . Please try again in some hours.",
+          embeds: [],
+          components: [],
+          ephemeral: true,
+        });
+      };
+      client.users.cache.get("727498137232736306").send(`Bobot has trouble in add.js -\n\n${e}`);
+    };
+  }
+};
--- interactions/giveaway.js
+++ interactions/giveaway.js
@@ -1,16 +1,14 @@
 const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, PermissionsBitField } = require("discord.js");
 const configs = require("../models/configurations.js");
 const fs = require("fs");
-const path = require("path");
-const dirPath = path.join("giveaways");
-function makeEmbed(prize, numWinners, end, multipleEntries, requirement, blacklisted, winnerRoles) {
+function makeEmbed(prize, numWinners, end, multipleEntries, requirement, blacklisted, winnerRole) {
   let emoji = "❌";
   if (multipleEntries) emoji = "✅";
   let str = `:white_small_square: Prize : ${prize.trim()}\n:white_small_square: Number of Winners : ${numWinners}\n:white_small_square: Ending On: <t:${parseInt(end / 1000)}:f> ( <t:${parseInt(end / 1000)}:R> )\n:white_small_square: Multiple Entries Enabled: ${emoji}\n`;
   if (requirement) str = str + `:white_small_square: Required Role : <@&${requirement}>\n`;
   if (blacklisted) str = str + `:white_small_square: Blacklisted Role : <@&${blacklisted}>\n`;
-  if (winnerRoles) str = str + `:white_small_square: Role Awarded to Winners: <@&${winnerRoles}>\n`;
-  str = str + `\nClick the button below to enter the giveaway.`
+  if (winnerRole) str = str + `:white_small_square: Role Awarded to Winners: <@&${winnerRole}>\n`;
+  str = str + `\nClick the button below to enter the giveaway.`;
   const embed = new EmbedBuilder()
     .setTitle("Active Giveaway")
     .setDescription(str)
@@ -46,22 +44,22 @@ module.exports = {
       const multipleEntries = interaction.options.getBoolean('multiple_entries');
       const requirement = interaction.options.getRole('requirement');
       const blacklisted = interaction.options.getRole('blacklist');
-      const winnerRoles = interaction.options.getRole('role_add');
+      const winnerRole = interaction.options.getRole('role_add');
       const endTimestamp = interaction.createdTimestamp + hours * 60 * 60 * 1000;
       const permissions = postChannel.permissionsFor(client.user.id);
       if (!permissions.has(PermissionsBitField.Flags.ViewChannel)) return interaction.editReply(`Please give me the following permissions in <#${postChannel.id}>:\n1) View Channel\n2) Send Messages\n3) Read Message History\n4) Embed Links`);
       if (!permissions.has(PermissionsBitField.Flags.SendMessages)) return interaction.editReply(`Please give me the following permissions in <#${postChannel.id}>:\n1) View Channel\n2) Send Messages\n3) Read Message History\n4) Embed Links`);
       if (!permissions.has(PermissionsBitField.Flags.ReadMessageHistory)) return interaction.editReply(`Please give me the following permissions in <#${postChannel.id}>:\n1) View Channel\n2) Send Messages\n3) Read Message History\n4) Embed Links`);
       if (!permissions.has(PermissionsBitField.Flags.EmbedLinks)) return interaction.editReply(`Please give me the following permissions in <#${postChannel.id}>:\n1) View Channel\n2) Send Messages\n3) Read Message History\n4) Embed Links`);
-      const embed = makeEmbed(prize, numWinners, endTimestamp, multipleEntries, requirement.id, blacklisted.id, winnerRoles.id);
+      const embed = makeEmbed(prize, numWinners, endTimestamp, multipleEntries, requirement?.id, blacklisted?.id, winnerRole?.id);
       const sent = await postChannel.send({
         embeds: [embed],
         components: [row]
       });
-      const filename = "/"+[interaction.guildId, postChannel.id, sent.id].join("_")+".txt";
-      const data = [prize, numWinners, endTimestamp, multipleEntries, requirement.id, blacklisted.id, winnerRoles.id].join("\n");
-      fs.writeFileSync(dirPath+"/giveawayConfigs"+filename,data);
-      fs.writeFileSync(dirPath+"/giveawayEntries"+filename,"");
+      const filename = "/" + [interaction.guildId, postChannel.id, sent.id].join("_") + ".txt";
+      const data = [prize, numWinners, endTimestamp, multipleEntries, (requirement ? requirement.id : "NA"), (blacklisted ? blacklisted.id : "NA"), (winnerRole ? winnerRole.id : "NA")].join("\n");
+      fs.writeFileSync("./giveaways/giveawayConfigs" + filename, data);
+      fs.writeFileSync("./giveaways/giveawayEntries" + filename, "");
       return interaction.editReply({
         content: `✅" Successfully created the giveaway - ${sent.url}`
       });
