f11a0b449574aaf8fdc8582be221c865949c1218
bo <akshatksyp13@gmail.com>
2022-08-02 18:11:58 +0200

    bugz

--- interactions/check.js
+++ interactions/check.js
@@ -9,7 +9,7 @@ module.exports = {
       const sub = subs.findOne({
         server_id: interaction.guildId,
       });
-      if(!sub) return interaction.editReply("The subscription for this server has expired, please renew at the [BoBot Labs Support Server](https://discord.gg/HweZtrzAnX) to continue using the services.");
+      if (!sub) return interaction.editReply("The subscription for this server has expired, please renew at the [BoBot Labs Support Server](https://discord.gg/HweZtrzAnX) to continue using the services.");
       const find = await wallets.findOne({
         discord_id: interaction.user.id,
         server_id: interaction.guildId,
@@ -24,6 +24,7 @@ module.exports = {
         return interaction.editReply({
           content: `The wallet address :\n**${wallet}**\n[Opensea](https://opensea.io/${wallet} 'Click to open opensea page for this wallet address.')\n[Etherscan](https://etherscan.io/address/${wallet} 'Click to open etherscan page for this wallet address.')\nis set up by you for this server and will be automatically submitted when you win a raffle in this server!`,
           ephemeral: true,
+          embeds: [],
         });
       };
     } catch (e) {
