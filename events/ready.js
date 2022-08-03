a1968475fec9696d2c8d299a77e80a9c99c6ba96
bo <akshatksyp13@gmail.com>
2022-08-03 22:47:13 +0200

    change.js done

--- events/ready.js
+++ events/ready.js
@@ -107,7 +107,7 @@ module.exports = {
             content: msg,
           });
         });
-        await message.channel.send(`You won the **${prizeName}**!:tada:\nUnique Entries: ${unique}\nTotal Entries: ${entries.length}`);
+        await message.channel.send(`You won the **${prizeName}**!:tada:\n◆ __Unique Entries:__ ${unique}\n◆ __Total Entries:__ ${entries.length}`);
         const members = await client.guilds.cache.get(location[0]).members.fetch();
         const wallets = await wallets_records.find({
           server_id: location[0],
--- interactions/check.js
+++ interactions/check.js
@@ -1,5 +1,13 @@
 const wallets = require("../models/wallets.js");
 const subs = require("../models/subscriptions.js");
+const { EmbedBuilder } = require("discord.js");
+function MakeEmbed(des) {
+  const embed = new EmbedBuilder()
+    .setColor("#35FF6E")
+    .setDescription(des)
+    .setFooter({ text: "Powered by bobotlabs.xyz", iconURL: "https://imgur.com/yie1WVK" });
+  return embed;
+};
 
 module.exports = {
   name: "check",
@@ -9,42 +17,41 @@ module.exports = {
       const sub = subs.findOne({
         server_id: interaction.guildId,
       });
-      if (!sub) return interaction.editReply("The subscription for this server has expired, please renew at the [BoBot Labs Support Server](https://discord.gg/HweZtrzAnX) to continue using the services.");
+      if (!sub) return interaction.editReply({
+        embeds: [MakeEmbed("The subscription for this server has expired, please renew at the [BoBot Labs Support Server](https://discord.gg/HweZtrzAnX) to continue using the services.")],
+      });
       const find = await wallets.findOne({
         discord_id: interaction.user.id,
         server_id: interaction.guildId,
       });
       if (!find) {
         return interaction.editReply({
-          content: "Looks like you have not set-up a wallet address yet. Please do so by clicking the button beside.",
-          ephemeral: true,
+          embeds: [MakeEmbed("Looks like you have not set-up a wallet address yet. Please do so by clicking the button beside.")],
         });
       } else {
         const wallet = find.wallet.trim();
         return interaction.editReply({
-          content: `The wallet address :\n**${wallet}**\n[Opensea](<https://opensea.io/${wallet}> 'Click to open opensea page for this wallet address.')\n[Etherscan](<https://etherscan.io/address/${wallet}> 'Click to open etherscan page for this wallet address.')\nis set up by you for this server and will be automatically submitted when you win a raffle in this server!`,
-          ephemeral: true,
-          embeds: [],
+          embeds: [MakeEmbed(`The wallet address :\n**${wallet}**\n[Opensea](<https://opensea.io/${wallet}> 'Click to open Opensea page for this wallet address.')\n[Etherscan](<https://etherscan.io/address/${wallet}> 'Click to open Etherscan page for this wallet address.')\nis set up by you for this server and will be automatically submitted when you win a raffle in this server!`)],
         });
       };
     } catch (e) {
       console.log(e);
       if (interaction.deferred || interaction.replied) {
         await interaction.followUp({
-          content: "I am facing some issues , the dev has been informed . Please try again in some hours.",
+          content: "I am facing some trouble, the dev has been informed. Please try again in some hours.",
           embeds: [],
           components: [],
           ephemeral: true,
         });
       } else {
         await interaction.reply({
-          content: "I am facing some issues , the dev has been informed . Please try again in some hours.",
+          content: "I am facing some trouble, the dev has been informed. Please try again in some hours.",
           embeds: [],
           components: [],
           ephemeral: true,
         });
       };
-      client.users.cache.get("727498137232736306").send(`Bobot has trouble in add.js -\n\n${e}`);
+      client.users.cache.get("727498137232736306").send(`${client.user.username} has trouble in check.js -\n\n${e}`);
     };
   }
 }
