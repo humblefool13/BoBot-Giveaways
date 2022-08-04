fb08a72a7579c000a46e2e9aef047d8c492cea55
bo <akshatksyp13@gmail.com>
2022-08-04 18:28:56 +0200

    editz

--- interactions/help.js
+++ interactions/help.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "help",
   async interact(client, interaction) {
     interaction.reply({
-      content: "soon",
+      content: "coming soon lol",
       ephemeral: true,
     });
   }
