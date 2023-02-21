070094f9dd8d644beb756ad77e2b0be87cc81b25
humblefool13 <akshatksyp13@gmail.com>
2023-02-22 03:13:04 +0530

    to adapt to new settings

--- models/configurations.js
+++ models/configurations.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const format = {
   server_id: String,
-  expired : Boolean,
+  expired: Boolean,
   expired_timestamp: Number,
   role: String,
   submit_channel: String,
+  winnners_channel: String,
+  server_timezone: String,
 };
 
 module.exports = mongoose.model('configs', format);
