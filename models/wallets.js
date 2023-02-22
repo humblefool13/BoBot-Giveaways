83dfb8b78864416cdac087e0f8f2f8957f8af055
humblefool13 <akshatksyp13@gmail.com>
2023-02-22 23:36:33 +0530

    new schema

--- models/wallets.js
+++ models/wallets.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 
 const format = {
   discord_id: String,
-  wallet_global: String,
-  wallets: [[]],
+  wallet_global_eth: String,
+  wallet_global_sol: String,
+  wallet_global_apt: String,
+  wallet_global_mulx: String,
+  wallets_eth: [[]],
+  wallets_sol: [[]],
+  wallets_apt: [[]],
+  wallets_mulx: [[]],
 };
 
 module.exports = mongoose.model('wallets', format);
