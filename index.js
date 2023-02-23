ca98f9eccc6ced2183ec1be361f61cbbd0ce2b5b
humblefool13 <akshatksyp13@gmail.com>
2023-02-24 02:24:14 +0530

    storing username + formatting

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post('/post', async (req, res) => {
   });
   const twitterUserInfoResult = await twitterUserInfoResponse.json();
   const twitterId = twitterUserInfoResult.data.id;
-  const twitterUsername = twitterUserInfoResponse.data.username;
+  const twitterUsername = twitterUserInfoResult.data.username;
   const discordUserInfoResponse = await fetch('https://discord.com/api/users/@me', {
     headers: {
       'Authorization': `Bearer ${discordAccessToken}`
@@ -96,8 +96,8 @@ app.post('/post', async (req, res) => {
   });
   const discordUserInfoResult = await discordUserInfoResponse.json();
   const discordId = discordUserInfoResult.id;
-  const discordUsername = discordUserInfoResponse.username;
-  const discordDiscriminator = discordUserInfoResponse.discriminator;
+  const discordUsername = discordUserInfoResult.username;
+  const discordDiscriminator = discordUserInfoResult.discriminator;
   const find = await twitter_db.findOne({
     twitter_id: twitterId,
   });
@@ -105,8 +105,8 @@ app.post('/post', async (req, res) => {
     await new twitter_db({
       twitter_id: twitterId,
       discord_id: discordId,
-      twitter_username: "@"+twitterUsername,
-      discord_username: discordUsername+"#"+discordDiscriminator,
+      twitter_username: "@" + twitterUsername,
+      discord_username: discordUsername + "#" + discordDiscriminator,
       access_token_twitter: encrypt(twitterAccessToken),
       access_token_discord: encrypt(discordAccessToken),
       refresh_token_discord: encrypt(discordRefreshToken),
@@ -119,8 +119,8 @@ app.post('/post', async (req, res) => {
       await new twitter_db({
         twitter_id: twitterId,
         discord_id: discordId,
-        twitter_username: "@"+twitterUsername,
-        discord_username: discordUsername+"#"+discordDiscriminator,
+        twitter_username: "@" + twitterUsername,
+        discord_username: discordUsername + "#" + discordDiscriminator,
         access_token_twitter: encrypt(twitterAccessToken),
         access_token_discord: encrypt(discordAccessToken),
         refresh_token_discord: encrypt(discordRefreshToken),
--- models/twitter.js
+++ models/twitter.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const format = {
   discord_id: String,
   twitter_id: String,
-  auth_token_twitter: String,
-  auth_token_discord: String,
+  twitter_username: String,
+  discord_username: String,
   access_token_twitter: String,
   access_token_discord: String,
   refresh_token_twitter: String,
