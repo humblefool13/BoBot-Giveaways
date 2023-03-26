5f94fe8dbaf82ad78b4fc77db08b81f27dca5ac6
bo <akshatksyp13@gmail.com>
2023-03-26 06:17:37 +0200

    default settings bug

--- interactions/defaults.js
+++ interactions/defaults.js
@@ -40,11 +40,11 @@ module.exports = {
           content: `Only <@&${managerRole}> can use this command.`,
         });
       const botRole = interaction.guild.members.me.roles.botRole;
-      let ping = interaction.options.getString('ping-role');
+      let ping = interaction.options.getString('ping-message');
       let blacklistedRoles = interaction.options.getString('blacklist-roles');
       let bonus = interaction.options.getString('bonus-entries');
       let balReq = interaction.options.getNumber('minimum-balance');
-      let reqRoles = interaction.options.getString('req-roles');
+      let reqRoles = interaction.options.getString('roles-req');
       let winnerRole = interaction.options.getRole('winner-role-add');
       if (
         !ping &&
